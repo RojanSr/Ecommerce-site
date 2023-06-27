1c57c9d242f4fa2e9d775cde5cbb4127631179e7
RojanSr <rairojan1116@gmail.com>
2023-06-27 12:12:32 +0545

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Box, Heading } from "@chakra-ui/react";
+import Home from "./components/Home/Home.jsx";
+
+export default function App() {
+  return (
+    <Box>
+      <Heading>App</Heading>
+      <Home />
+    </Box>
+  );
+}
---
+++ src/components/Header/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Box, Flex } from "@chakra-ui/react";
+
+export default function Navbar() {
+  return (
+    <Box>
+      <Flex></Flex>
+    </Box>
+  );
+}
