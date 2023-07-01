6dec45926804bd833fbd18c34c2d2681d98b77ac
RojanSr <rairojan1116@gmail.com>
2023-07-01 20:52:16 +0545

    add: wrap the cards for responsiveness

--- src/page/Home/NewArrival/index.jsx
+++ src/page/Home/NewArrival/index.jsx
@@ -9,7 +9,11 @@ import imgHolder4 from "../../../../assets/NewArrival/Full_Sleeve.jpg";
 export default function NewArrival() {
   return (
     <Box mx={["30px", "40px", "40px", "80px"]}>
-      <Flex justifyContent="center" gap="70px">
+      <Flex
+        justifyContent="center"
+        gap={{ base: "40px", lg: "70px" }}
+        flexWrap="wrap"
+      >
         <ArrivedCard imgSrc={imgHolder1} name="Knitted Joggers" />
         <ArrivedCard imgSrc={imgHolder2} name="Active T-Shirts" />
         <ArrivedCard imgSrc={imgHolder3} name="Urban Shirts" />
