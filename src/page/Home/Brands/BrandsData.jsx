bda228624c5a137b159d6b45305b7d3c49e60536
RojanSr <rairojan1116@gmail.com>
2023-07-02 14:42:28 +0545

    store: brand images locally (use api in future)

---
+++ src/page/Home/Brands/BrandsData.jsx
@@ -0,0 +1,9 @@
+import img1 from "../../../../assets/Brands/Nike.jpg";
+import img2 from "../../../../assets/Brands/HM.jpg";
+import img3 from "../../../../assets/Brands/Levis.jpg";
+import img4 from "../../../../assets/Brands/Polo.png";
+import img5 from "../../../../assets/Brands/Puma.jpg";
+
+const brandData = [img1, img2, img3, img4, img5];
+
+export default brandData;
