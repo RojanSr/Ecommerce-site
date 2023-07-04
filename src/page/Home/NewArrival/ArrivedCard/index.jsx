3841dc096eb902ce65dac07eef7c506225c085d6
RojanSr <rairojan1116@gmail.com>
2023-07-04 20:24:37 +0545

    refactor style: add div over image

--- src/page/Home/NewArrival/ArrivedCard/index.jsx
+++ src/page/Home/NewArrival/ArrivedCard/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Box, Image, Text } from "@chakra-ui/react";
 
-export default function ArrivedCard({ imgSrc, name }) {
+export default function ArrivedCard({ imgSrc, name, id }) {
   return (
     <Box
       minW={{ base: "100px", md: "150px", lg: "200px" }}
@@ -9,14 +9,21 @@ export default function ArrivedCard({ imgSrc, name }) {
       cursor="pointer"
       transition="0.2s ease-in-out"
       _hover={{
-        transform: "scale(1.1)",
+        transform: "scale(1.05)",
       }}
     >
-      <Image src={imgSrc} width="100%" alt="Newly Arrived Clothes" />
+      <Box h={{ base: "150px", lg: "300px" }}>
+        <Image
+          src={imgSrc}
+          width="100%"
+          alt="Newly Arrived Clothes"
+          objectFit="contain"
+        />
+      </Box>
       <Text
         mt={{ base: "10px", lg: "33px" }}
         color="#3C4242"
-        fontSize={{ base: "14px", lg: "20px" }}
+        fontSize={{ base: "14px", lg: "18px" }}
         fontWeight="700"
         letterSpacing="0.2px"
       >
