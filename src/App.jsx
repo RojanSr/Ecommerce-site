4efaf2b005761b9edba0164b77947e80c46a7e9d
RojanSr <rairojan1116@gmail.com>
2023-07-04 20:23:04 +0545

    add: context to api

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { Box } from "@chakra-ui/react";
 import Navbar from "./page/Header/Navbar.jsx";
 import Home from "./page/Home/Home.jsx";
 import Footer from "./components/Footer";
 
+export const ProductContext = React.createContext();
 export default function App() {
+  const [products, setProducts] = useState({});
+
+  useEffect(() => {
+    axios
+      .get("https://fakestoreapi.com/products")
+      .then((res) => setProducts(res));
+  }, []);
+
   return (
     <Box minW="100vw" maxW="100vw" overflow="hidden">
       <Navbar />
-      <Home />
+      <ProductContext.Provider value={products}>
+        <Home />
+      </ProductContext.Provider>
       <Footer />
     </Box>
   );
