e17c5d6ee5c81f169a940897cfaad04eead320af
RojanSr <rairojan1116@gmail.com>
2023-07-04 20:24:04 +0545

    update: use context to retrieve data

--- src/page/Home/NewArrival/index.jsx
+++ src/page/Home/NewArrival/index.jsx
@@ -1,23 +1,40 @@
-import React from "react";
+import React, { useContext } from "react";
 import ArrivedCard from "./ArrivedCard";
 import { Flex, Box } from "@chakra-ui/react";
-import imgHolder1 from "../../../../assets/NewArrival/Knitted_Joggers.jpg";
-import imgHolder2 from "../../../../assets/NewArrival/Active_TShirt.jpg";
-import imgHolder3 from "../../../../assets/NewArrival/Urban_Shirts.jpg";
-import imgHolder4 from "../../../../assets/NewArrival/Full_Sleeve.jpg";
+import { ProductContext } from "../../../App";
 
 export default function NewArrival() {
+  const product = useContext(ProductContext);
   return (
     <Box mx={["30px", "40px", "40px", "80px"]}>
       <Flex
         justifyContent="center"
-        gap={{ base: "40px", lg: "70px" }}
+        gap={{ base: "60px", lg: "100px" }}
         flexWrap="wrap"
       >
-        <ArrivedCard imgSrc={imgHolder1} name="Knitted Joggers" />
-        <ArrivedCard imgSrc={imgHolder2} name="Active T-Shirts" />
-        <ArrivedCard imgSrc={imgHolder3} name="Urban Shirts" />
-        <ArrivedCard imgSrc={imgHolder4} name="Full Sleeves" />
+        {/* {data.map((product, index) => (
+          <ArrivedCard
+            imgSrc={product.imgSrc}
+            name={product.name}
+            id={product.id}
+            key={index}
+          />
+        ))} */}
+
+        {/* Only includes first 5 items */}
+        {product.data.map((item, index) => {
+          if (index > 3) {
+            return;
+          }
+          return (
+            <ArrivedCard
+              name={item.title}
+              imgSrc={item.image}
+              id={item.id}
+              key={index}
+            />
+          );
+        })}
       </Flex>
     </Box>
   );
