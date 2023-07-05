883c0ecdadf15712e9bb4b9c8739aba37b8062aa
RojanSr <rairojan1116@gmail.com>
2023-07-05 15:04:07 +0545

    add: page routes to view product

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,58 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import { Box } from "@chakra-ui/react";
 import Navbar from "./page/Header/Navbar.jsx";
 import Home from "./page/Home/Home.jsx";
 import Footer from "./components/Footer";
 
+import { Route, Routes } from "react-router-dom";
+import ProductView from "./page/ProductView/index.jsx";
+
 export const ProductContext = React.createContext();
 export default function App() {
-  const [products, setProducts] = useState({});
+  const [products, setProducts] = useState([]);
 
+  // Fetching data from fakestoreapi.com
   useEffect(() => {
-    axios
-      .get("https://fakestoreapi.com/products")
-      .then((res) => setProducts(res));
+    async function fetchData() {
+      try {
+        const res = await fetch("https://fakestoreapi.com/products");
+
+        if (!res.ok) {
+          throw new Error("Error while fetch");
+        }
+        const data = await res.json();
+
+        setProducts(data);
+      } catch (error) {
+        console.error("Error fetching data", error);
+        return null;
+      }
+    }
+
+    fetchData();
   }, []);
 
   return (
     <Box minW="100vw" maxW="100vw" overflow="hidden">
       <Navbar />
-      <ProductContext.Provider value={products}>
-        <Home />
-      </ProductContext.Provider>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ProductContext.Provider value={products ? products : []}>
+              <Home />
+            </ProductContext.Provider>
+          }
+        />
+        <Route
+          path="/ProductView/:id"
+          element={
+            <ProductContext.Provider value={products ? products : []}>
+              <ProductView />
+            </ProductContext.Provider>
+          }
+        />
+      </Routes>
       <Footer />
     </Box>
   );
