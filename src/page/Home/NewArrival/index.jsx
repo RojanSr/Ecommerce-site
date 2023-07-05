a778cc13a48fa12ac3da574c450bf0f5a325eaf0
RojanSr <rairojan1116@gmail.com>
2023-07-05 15:07:18 +0545

    fix: product is undefined issue

--- src/page/Home/NewArrival/index.jsx
+++ src/page/Home/NewArrival/index.jsx
@@ -5,6 +5,7 @@ import { ProductContext } from "../../../App";
 
 export default function NewArrival() {
   const product = useContext(ProductContext);
+
   return (
     <Box mx={["30px", "40px", "40px", "80px"]}>
       <Flex
@@ -12,29 +13,22 @@ export default function NewArrival() {
         gap={{ base: "60px", lg: "100px" }}
         flexWrap="wrap"
       >
-        {/* {data.map((product, index) => (
-          <ArrivedCard
-            imgSrc={product.imgSrc}
-            name={product.name}
-            id={product.id}
-            key={index}
-          />
-        ))} */}
-
         {/* Only includes first 5 items */}
-        {product.data.map((item, index) => {
-          if (index > 3) {
-            return;
-          }
-          return (
-            <ArrivedCard
-              name={item.title}
-              imgSrc={item.image}
-              id={item.id}
-              key={index}
-            />
-          );
-        })}
+
+        {typeof product &&
+          product.map((item, index) => {
+            if (index > 3) {
+              return;
+            }
+            return (
+              <ArrivedCard
+                name={item.title}
+                imgSrc={item.image}
+                id={item.id}
+                key={index}
+              />
+            );
+          })}
       </Flex>
     </Box>
   );
