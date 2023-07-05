1d69a85d01b94153db0260345f0a988e7ec429be
RojanSr <rairojan1116@gmail.com>
2023-07-05 20:37:04 +0545

    create: Combo page with all products displayed

---
+++ src/page/Combo/Combo.jsx
@@ -0,0 +1,29 @@
+import React, { useContext } from "react";
+import { ProductContext } from "../../App";
+import { Box, Flex, Heading } from "@chakra-ui/react";
+import ContentTitle from "../../components/ContentTitle";
+import Card from "../../components/Card";
+
+export default function Combo() {
+  // Retrieving through context
+  const products = useContext(ProductContext);
+
+  return (
+    <Box>
+      <ContentTitle text="Combo" />
+      <Flex wrap="wrap" gap="50" justifyContent="center" mx={10}>
+        {products.map((el, index) => {
+          return (
+            <Card
+              imgSrc={el.image}
+              name={el.title}
+              id={el.id}
+              key={index}
+              price={el.price}
+            />
+          );
+        })}
+      </Flex>
+    </Box>
+  );
+}
