1fb02e6e6fa462ee5979487a3e1c730a8291740f
RojanSr <rairojan1116@gmail.com>
2023-07-05 15:11:39 +0545

    create: component to be able to select sizes

---
+++ src/page/ProductView/ProductDetails/SizeSelect.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import { Text, Flex } from "@chakra-ui/react";
+
+export default function SizeSelect() {
+  const sizeArr = ["XS", "S", "M", "L", "XL"];
+
+  // Initialize state with default value as "M"
+  const [size, setSize] = useState("M");
+
+  // Toggle between sizes
+  function toggleSize(index) {
+    setSize(sizeArr[index]);
+  }
+  return (
+    <>
+      <Text fontSize="18px" m="30px 0px 10px 0px">
+        Select Size
+      </Text>
+      <Flex gap="20px">
+        {sizeArr.map((el, index) => (
+          <Flex
+            key={index}
+            justifyContent="center"
+            alignItems="center"
+            h="38px"
+            w="38px"
+            border="1.5px solid #BEBCBD"
+            borderRadius="12px"
+            cursor="pointer"
+            onClick={() => toggleSize(index)}
+            // Check if the element value is same as the state variable value
+            bgColor={el == size ? "#3C4242" : "inherit"}
+            color={el == size ? "#fff" : "inherit"}
+          >
+            {el}
+          </Flex>
+        ))}
+      </Flex>
+    </>
+  );
+}
