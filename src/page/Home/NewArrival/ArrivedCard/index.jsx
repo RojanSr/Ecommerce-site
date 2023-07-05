c19d4f82ede76f0fc544192db3bf2264b9d95d38
RojanSr <rairojan1116@gmail.com>
2023-07-05 15:08:58 +0545

    add: router link to view product by dynamic route

--- src/page/Home/NewArrival/ArrivedCard/index.jsx
+++ src/page/Home/NewArrival/ArrivedCard/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Box, Image, Text } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
 export default function ArrivedCard({ imgSrc, name, id }) {
   return (
@@ -12,23 +13,25 @@ export default function ArrivedCard({ imgSrc, name, id }) {
         transform: "scale(1.05)",
       }}
     >
-      <Box h={{ base: "150px", lg: "300px" }}>
-        <Image
-          src={imgSrc}
-          width="100%"
-          alt="Newly Arrived Clothes"
-          objectFit="contain"
-        />
-      </Box>
-      <Text
-        mt={{ base: "10px", lg: "33px" }}
-        color="#3C4242"
-        fontSize={{ base: "14px", lg: "18px" }}
-        fontWeight="700"
-        letterSpacing="0.2px"
-      >
-        {name}
-      </Text>
+      <Link to={`/ProductView/${id}`}>
+        <Box h={{ base: "150px", lg: "300px" }}>
+          <Image
+            src={imgSrc}
+            width="100%"
+            alt="Newly Arrived Clothes"
+            objectFit="contain"
+          />
+        </Box>
+        <Text
+          mt={{ base: "10px", lg: "33px" }}
+          color="#3C4242"
+          fontSize={{ base: "14px", lg: "18px" }}
+          fontWeight="700"
+          letterSpacing="0.2px"
+        >
+          {name}
+        </Text>
+      </Link>
     </Box>
   );
 }
