f2ef620fa3c57f7e963ecfc557fb8e1e35623f72
RojanSr <rairojan1116@gmail.com>
2023-07-06 18:11:40 +0545

    create: new page

---
+++ src/page/Men/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ContentTitle from "../../components/ContentTitle";
+import { Box } from "@chakra-ui/react";
+
+export default function Men() {
+  return (
+    <Box>
+      <ContentTitle text="Men" />
+    </Box>
+  );
+}
---
+++ src/page/Women/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ContentTitle from "../../components/ContentTitle";
+import { Box } from "@chakra-ui/react";
+
+export default function Women() {
+  return (
+    <Box>
+      <ContentTitle text="Women" />
+    </Box>
+  );
+}
