d136e7703e4220ecc059e71dcc2656ab8cdf038a
RojanSr <rairojan1116@gmail.com>
2023-07-07 21:55:29 +0545

    update: filter category and map to render

--- src/page/Men/index.jsx
+++ src/page/Men/index.jsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useContext } from "react";
 import ContentTitle from "../../components/ContentTitle";
-import { Box } from "@chakra-ui/react";
+import Card from "../../components/Card";
+import { Box, Flex } from "@chakra-ui/react";
+import { ProductContext } from "../ProductContext";
 
 export default function Men() {
+  const products = useContext(ProductContext);
+
+  const menProducts = products.filter((el) => el.category == "men's clothing");
+
   return (
     <Box>
       <ContentTitle text="Men" />
+      <Flex wrap="wrap" gap="50" justifyContent="center" mx={10}>
+        {menProducts.map((el, index) => {
+          return (
+            <Card
+              imgSrc={el.image}
+              name={el.title}
+              id={el.id}
+              key={index}
+              price={el.price}
+            />
+          );
+        })}
+      </Flex>
     </Box>
   );
 }
--- src/page/Women/index.jsx
+++ src/page/Women/index.jsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useContext } from "react";
 import ContentTitle from "../../components/ContentTitle";
-import { Box } from "@chakra-ui/react";
+import Card from "../../components/Card";
+import { Box, Flex } from "@chakra-ui/react";
+import { ProductContext } from "../ProductContext";
 
 export default function Women() {
+  const products = useContext(ProductContext);
+
+  const womenProducts = products.filter(
+    (el) => el.category == "women's clothing"
+  );
+
   return (
     <Box>
       <ContentTitle text="Women" />
+      <Flex wrap="wrap" gap="50" justifyContent="center" mx={10}>
+        {womenProducts.map((el, index) => {
+          return (
+            <Card
+              imgSrc={el.image}
+              name={el.title}
+              id={el.id}
+              key={index}
+              price={el.price}
+            />
+          );
+        })}
+      </Flex>
     </Box>
   );
 }
